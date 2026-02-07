9b1711781370dac18f93b67691701983ee4182b9
ryoga-matsumura <ry0ga132@gmail.com>
2026-02-07 16:16:00 +0900

    最初のコミット。虚無の進捗バー完成。

--- src/main/java/com/example/VoidProgressBar/Controller/PageController.java
+++ src/main/java/com/example/VoidProgressBar/Controller/PageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PageController {
 
   // ブラウザで http://localhost:8080/void にアクセスした時の処理
-  @GetMapping("/void")
+  @GetMapping("/")
   public String showVoidPage() {
     // resources/templates/void.html を表示しなさい、という意味
     return "void";
